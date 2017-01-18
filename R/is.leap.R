aa8e89bec20f06083f5bdc248c54d23a61fb2b86
Wilmer van der Spek <Wilmer van der Spek>
2017-01-18 21:32:13 +0100

    ex3

--- R/is.leap.R
+++ R/is.leap.R
@@ -1,4 +1,10 @@
 
 is.leap <- function(x) {
+  if(class(x) != "numeric"){
+    return('Error: argument of class numeric expected')
+  } 
+  if(x < 1600){
+    return('That year is out of the valid range')
+  }
   return(((x %% 4 == 0) & (x %% 100 != 0)) | (x %% 400 == 0))
 }
--- main.R
+++ main.R
@@ -1,6 +1,10 @@
 # Wilmer van der Spek
 # 18-01-2017
 
-source('R/isleap.R')
+source('R/is.leap.R')
 
 is.leap(2000)
+
+is.leap("Hello")
+
+is.leap(1400)
